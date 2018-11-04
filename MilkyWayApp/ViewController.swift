d1f57851e236ef9c31c4ae5ddb496e8b5b6e22b4
Emese Tóth <temsethebest@gmail.com>
2018-11-04 12:51:35 +0000

    Initial Commit

---
+++ MilkyWayApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MilkyWayApp
+//
+//  Created by David Tverdota on 04/11/2018.
+//  Copyright © 2018 Emese Toth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
